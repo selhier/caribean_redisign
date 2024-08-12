eaf2174e6cbf36bdcbe19bc15fbaa5fc0d018415
Haryko <antonio.r1894@gmail.com>
2024-08-12 16:43:58 -0400

    Ejemplos

---
+++ Caribbean_redisign/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import reactLogo from './assets/react.svg';
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nisi, hic possimus cumque aut porro repellendus architecto similique, dolore obcaecati quae sequi iusto qui itaque fugit? Dolores veritatis quam vitae.</p>
+            <img src={reactLogo} alt="" />
+        </footer>
+    )    
+}
+export default Footer;
---
+++ Caribbean_redisign/src/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Nabvar = () => {
+    return (
+        <nav className="navbar">
+            <div>
+             <a href="">ALexander</a>
+             <a href="">Hola</a>
+            </div>
+            <div>
+            </div>
+        </nav>
+    )
+};
+export default Nabvar;
