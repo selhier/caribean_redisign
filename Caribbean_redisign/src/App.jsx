608f61d6a8f72e2d60a0dbac36fd70b4c2dcc059
AlexEscano <alexandermatos7887@gmail.com>
2024-08-14 16:59:55 -0400

    Commit by Alex

--- Caribbean_redisign/src/App.jsx
+++ Caribbean_redisign/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Nabvar from './Nav'
+import Hero from './hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <Nabvar/>
+    <Nabvar/>
+    <Hero/>
     </>
   )
 }
--- Caribbean_redisign/src/hero.jsx
+++ Caribbean_redisign/src/hero.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
 const Hero = ()=>{
+
     return(
         <section className="hero">
             
         </section>
     )
 }
+
+export default Hero;
