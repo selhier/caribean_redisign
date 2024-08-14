2d76ba5c8cfdb66dce351f5b6cdeb04f31d2abd8
Haryko <antonio.r1894@gmail.com>
2024-08-14 11:41:14 -0400

    instalacion correcta de tailwind

--- Caribbean_redisign/src/App.jsx
+++ Caribbean_redisign/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Navbar from './Nav'
 import './App.css'
-import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar />
-    <Footer />
+   <Nabvar/>
     </>
   )
 }
--- Caribbean_redisign/src/Nav.jsx
+++ Caribbean_redisign/src/Nav.jsx
@@ -4,12 +4,9 @@ import React from "react";
 const Nabvar = () => {
     return (
         <nav className="navbar">
-            <div>
-             <a href="">ALexander</a>
-             <a href="">Hola</a>
-            </div>
-            <div>
-            </div>
+            <header>
+                
+            </header>
         </nav>
     )
 };
