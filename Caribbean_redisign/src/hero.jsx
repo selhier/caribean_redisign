3008c451ab6653ed3f53407496607e2d0a23c45f
AlexEscano <alexandermatos7887@gmail.com>
2024-08-15 08:04:14 -0400

    hero_modificacion

--- Caribbean_redisign/src/hero.jsx
+++ Caribbean_redisign/src/hero.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Hero = ()=>{
-
+    const hero = "https://images2.alphacoders.com/136/1363754.jpeg";
     return(
         <section className="hero">
-            
+            <img className="hero__img" src={hero} alt="" />
+            <div className="hero__content">
+                <h1 className="hero__content--titulo">Deadpool & Wolverine</h1>
+            </div>
         </section>
     )
 }
