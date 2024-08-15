93572f7e56a6589ae9c0d410bd0a091e8c272e8e
Selhier <harykoshinigami@gmail.com>
2024-08-15 00:45:06 -0400

    css navbar

--- Caribbean_redisign/src/Nav.jsx
+++ Caribbean_redisign/src/Nav.jsx
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import './NavBar.css';
 
 const Nabvar = () => {
+  const [hoverStates, setHoverStates] = useState(
+    [
+      { id: 'inicio', isHovering: false },
+      { id: 'estrenos', isHovering: false },
+      { id: 'proximamente', isHovering: false },
+      { id: 'cartelera', isHovering: false },
+      { id: 'fineArts', isHovering: false },
+      { id: 'cumpleaños', isHovering: false },
+      { id: 'salasVIP', isHovering: false },
+      { id: 'cinemaEvents', isHovering: false },
+      { id: 'formatos', isHovering: false },
+    ]
+  );
+  const handleMouseEnter = (id) => {
+    setHoverStates(prevStates =>
+      prevStates.map(state =>
+        state.id === id ? { ...state, isHovering: true } : state
+      )
+    );
+  };
+
+  const handleMouseLeave = (id) => {
+    setHoverStates(prevStates =>
+      prevStates.map(state =>
+        state.id === id ? { ...state, isHovering: false } : state
+      )
+    );
+  };
     return (
-        
-            <header className="header">
-                <div className="logo-cotainer">
-                    <img src={logo} alt="Logo" />
-                </div>
-                {/* links de navegación */}
-      <nav>
-      <ul className="nav-container">
-        <li>
-          <a href="/">Inicio</a>
-        </li>
-        <li>
-          <a href="/">Productos</a>
-        </li>
-        <li>
-          <a href="/">Blog</a>
-        </li>
-        <li>
-          <a href="/">Contacto</a>
-        </li>
-      </ul>
-    </nav>
-        </header>
-        
+        <div className="nav-container">
+      <ul className="nav-list">
+          {hoverStates.map(state => (
+            <li key={state.id}>
+              <a href="/"
+                 onMouseEnter={() => handleMouseEnter(state.id)}
+                 onMouseLeave={() => handleMouseLeave(state.id)}
+                 className={state.isHovering ? 'nav-link active' : 'nav-link'}>
+                {state.id}
+              </a>
+            </li>
+          ))}
+        </ul>
+    </div>
     )
 };
+
 export default Nabvar;
+
